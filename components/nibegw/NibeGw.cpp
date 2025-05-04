613259af7eec60fceeb6b63a34f80ad247d960a8
Malte Krüger <M@lteKrueger.de>
2025-05-04 20:25:29 +0200

    reduced log level

--- components/nibegw/NibeGw.cpp
+++ components/nibegw/NibeGw.cpp
@@ -110,7 +110,7 @@ void NibeGw::loop() {
     case STATE_WAIT_ACK:
       if (RS485->available() > 0) {
         byte b = RS485->read();
-        ESP_LOGDVV(TAG, "%02X", b);
+        ESP_LOGVV(TAG, "%02X", b);
 
         if (b == STARTBYTE_ACK) {
           ESP_LOGV(TAG, "Ack seen");
